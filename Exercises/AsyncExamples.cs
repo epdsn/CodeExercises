c7353a547f12a6d4ce951ae581411f4ce9300a0e
epdsn <ibestudios@gmail.com>
2025-11-25 07:41:44 -0700

    Added async class example

---
+++ Exercises/AsyncExamples.cs
@@ -0,0 +1,16 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class  AsyncExamples
+    {
+
+        public Task RunTimerExanpleAsync(CancellationToken cancellationToken)
+        { }
+
+        public Task RunConcurrentExampleAsync(CancellationToken cancellationToken)
+        { }
+        
+    }
+}
